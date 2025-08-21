259085cdea6241d20f45e132ef4bed97ea5a6ee2
youngji <youngji0201@gmail.com>
2025-08-22 01:35:17 +0900

    news update

--- src/data/news.ts
+++ src/data/news.ts
@@ -10,12 +10,12 @@ export const news: NewsItem[] = [
     date: "2025-09-03",
     title: "K-Data Science Research Presentation Finalist 🏆",
     desc: "Advanced to the final round of the competition.",
-    link: "https://kdatascience.kr/hackathon/competition",
+    link: "https://kdatascience.kr/conference/event",
   },
   {
     date: "2025-08-01",
     title: "My First IMWUT Paper Accepted 🎉",
     desc: "Will present at Ubicomp 2025 in Finland.",
-    link: "#",
+    link: "https://www.ubicomp.org/ubicomp-iswc-2025/",
   },
 ];
